220d6d0a46dbbb77840832b842b0f635f3e39deb
NastyaTsimanovich97 <nastya.cimanovich.97@mail.ru>
2024-10-05 16:39:16 +0300

    feat(task-1): cp with spawn

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,9 +1,17 @@
-import { fork } from "child_process";
+import { fork, spawn } from "child_process";
+
+const __dirname = import.meta.dirname;
 
 const spawnChildProcess = async (args) => {
   // Write your code here
-  fork("./src/cp/files/script.js", args);
+
+  spawn("node", [`${__dirname}/files/script.js`, ...args], {
+    stdio: "inherit",
+  });
+
+  // -----OTHER SOLUTION WITH fork-----
+  // fork(`${__dirname}/files/script.js`, args);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess(["someArgument1", "someArgument2"]);
+spawnChildProcess(["someArgument1", "someArgument2", "test", "IPC", "fork"]);
